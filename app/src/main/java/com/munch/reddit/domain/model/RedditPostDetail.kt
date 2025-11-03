deb108082a880fd091aa74ecffc4bd1086a273f6
Naren Kukreja <narenkukreja@gmail.com>
2025-11-02 19:16:39 -0500

    tf

--- app/src/main/java/com/munch/reddit/domain/model/RedditPostDetail.kt
+++
@@ -1,7 +0,0 @@
-package com.munch.reddit.domain.model
-
-data class RedditPostDetail(
-    val post: RedditPost,
-    val comments: List<RedditComment>,
-    val nextCommentCursor: RedditCommentCursor?
-)
